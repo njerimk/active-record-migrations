12d2195c31c2b99aefc1f85ae4b7da404c9a7d39
njerimk <nmurugikamau@gmail.com>
2020-10-18 13:11:59 -0400

    Done.

---
+++ db/migrate/01_create_artists.rb
@@ -0,0 +1,32 @@
+class CreateArtists < ActiveRecord::Migration[5.2]
+    
+    def change 
+        create_table :artists do |t|
+            t.string :name 
+            t.string :genre
+            t.integer :age
+            t.string :hometown
+        end
+    end
+    
+    def change
+       sql = <<- SQL 
+        CREATE TABLE IF NOT EXISTS artists (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        genre TEXT,
+        age INTEGER,
+        hometown TEXT
+        )
+       SQL
+
+       ActiveRecord::Base.connection.execute(sql)
+    end
+    
+    def up
+    end
+
+    def down
+    end
+
+end
